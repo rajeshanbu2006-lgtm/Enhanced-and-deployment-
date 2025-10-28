87bd93e9bd3ebfdd83fbbd422a776ba497f86619
rajeshanbu2006-lgtm <rajeshanbu2006@gmail.com>
2025-10-28 11:29:19 +0530

    Create Frontend -node.js

---
+++ Frontend -node.js
@@ -0,0 +1,18 @@
+{
+  "name": "file-upload-frontend",
+  "version": "1.0.0",
+  "private": true,
+  "dependencies": {
+    "react": "^18.2.0",
+    "react-dom": "^18.2.0",
+    "uuid": "^9.0.0"
+  },
+  "scripts": {
+    "start": "vite",
+    "build": "vite build"
+  },
+  "devDependencies": {
+    "vite": "^5.0.0",
+    "@vitejs/plugin-react": "^4.0.0"
+  }
+}
